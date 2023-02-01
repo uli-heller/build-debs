f2e2d147a50e175c98c1138142eaa4488d224438
Uli Heller <uli.heller@daemons-point.com>
2023-02-01 07:46:35 +0100

    Sortierung der Thunderbird-Versionen korrigiert

--- any-ubuntu/thunderbird/download-thunderbird.sh
+++ any-ubuntu/thunderbird/download-thunderbird.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BASE_URL="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/"
-THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9.]*/"|sort -V|tail -1|xargs basename)"
+THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9.]*/"|xargs -n1 basename|sort -V|tail -1)"
 URL="${BASE_URL}${THUNDERBIRD_VERSION}/linux-x86_64/de/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
 if [ -s "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2" ]; then
   echo >&2 "Bereits vorhanden: '${URL}'"
