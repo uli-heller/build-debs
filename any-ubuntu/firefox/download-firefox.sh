1ba0e153923d4c9661c1f1911757e58a9bd75243
Uli Heller <uli.heller@daemons-point.com>
2023-02-01 07:44:40 +0100

    Sortierung der Firefox-Versionen korrigiert

--- any-ubuntu/firefox/download-firefox.sh
+++ any-ubuntu/firefox/download-firefox.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+#
+# Stand 2023-02-01 gibt es diese Firefox-Versionen:
+# - /pub/firefox/releases/108.0.1/
+# - /pub/firefox/releases/108.0.2/
+# - /pub/firefox/releases/108.0/
+# - /pub/firefox/releases/109.0.1/
+# - /pub/firefox/releases/109.0/
+# Bei "sort -V" gibt es gibt es die vorgenannte Sortierung.
+# Wenn ich nur die Versionsnummern ausschneide und diese
+# dann mit "sort -V" sortiere, dann gibt es diese Sortierung:
+# - 108.0
+# - 108.0.1
+# - 108.0.2
+# - 109.0
+# - 109.0.1
+# Also: ERST Versionsnummer ausschneiden, DANN sortieren!
+#
 BASE_URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/"
-FIREFOX_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/firefox/releases/[0-9.]*/"|sort -V|tail -1|xargs basename)"
+FIREFOX_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/firefox/releases/[0-9.]*/"|xargs -n1 basename|sort -V|tail -1)"
 URL="${BASE_URL}${FIREFOX_VERSION}/linux-x86_64/de/firefox-${FIREFOX_VERSION}.tar.bz2"
 if [ -s "${HOME}/Downloads/firefox-${FIREFOX_VERSION}.tar.bz2" ]; then
   echo >&2 "Bereits vorhanden: '${URL}'"
