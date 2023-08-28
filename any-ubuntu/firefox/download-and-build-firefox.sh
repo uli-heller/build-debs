dd0abb0cee51c3e16dbd41b4ac22acf42e6cc2d9
Uli Heller <uli.heller@daemons-point.com>
2023-08-28 18:17:59 +0200

    Wait for the virus check to complete

--- any-ubuntu/firefox/download-and-build-firefox.sh
+++ any-ubuntu/firefox/download-and-build-firefox.sh
@@ -6,5 +6,10 @@ T="$(readlink -f "$0")"
 TD="$(dirname "${T}")"
 
 TBZ2="$("${TD}/download-firefox.sh")" && {
+  # Warte, bis der Virencheck durch ist
+  for s in $(seq 1 100); do
+    sleep 1
+    test -r "${TBZ2}" && break
+  done
   "${TD}/build-firefox.sh" "${TBZ2}" "$1"
 }
--- any-ubuntu/thunderbird/download-and-build-thunderbird.sh
+++ any-ubuntu/thunderbird/download-and-build-thunderbird.sh
@@ -6,5 +6,10 @@ T="$(readlink -f "$0")"
 TD="$(dirname "${T}")"
 
 TBZ2="$("${TD}/download-thunderbird.sh")" && {
+  # Warte, bis der Virencheck durch ist
+  for s in $(seq 1 100); do
+    sleep 1
+    test -r "${TBZ2}" && break
+  done
   "${TD}/build-thunderbird.sh" "${TBZ2}" "$1"
 }
