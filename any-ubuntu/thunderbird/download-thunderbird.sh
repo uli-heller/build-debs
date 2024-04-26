5b4f83064a838fce556b92a534245d1c25af6816
Uli Heller <uli.heller@daemons-point.com>
2024-04-26 08:19:54 +0200

    Skip thunderbird 125

--- any-ubuntu/thunderbird/download-thunderbird.sh
+++ any-ubuntu/thunderbird/download-thunderbird.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+set -x
 BASE_URL="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/"
-THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9.]*/"|xargs -n1 basename|grep -v "thunderbird-125"|sort -V|tail -1)"
+THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9.]*/"|grep -v "/125"|xargs -n1 basename|sort -V|tail -1)"
 URL="${BASE_URL}${THUNDERBIRD_VERSION}/linux-x86_64/de/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
 if [ -s "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2" ]; then
   echo >&2 "Bereits vorhanden: '${URL}'"
