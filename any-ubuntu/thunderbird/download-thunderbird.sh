8fe67fd672c3f9ca6cc10f5b7e37aee5a6d81dc1
Uli Heller <uli.heller@daemons-point.com>
2024-12-02 08:27:40 +0100

    Skip thunderbird-133

--- any-ubuntu/thunderbird/download-thunderbird.sh
+++ any-ubuntu/thunderbird/download-thunderbird.sh
@@ -2,7 +2,7 @@
 
 #         https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/128.0esr/linux-x86_64/de/thunderbird-128.0esr.tar.bz2
 BASE_URL="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/"
-THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9][0-9.]*[^/]*/"|grep -v "/12[579]"|grep -v "/13[012]"|grep -v "b[^/]*/$"|xargs -n1 basename|sort -V|tail -1)"
+THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9][0-9.]*[^/]*/"|grep -v "/12[579]"|grep -v "/13[0123]"|grep -v "b[^/]*/$"|xargs -n1 basename|sort -V|tail -1)"
 URL="${BASE_URL}${THUNDERBIRD_VERSION}/linux-x86_64/de/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
 if [ -s "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2" ]; then
   echo >&2 "Bereits vorhanden: '${URL}'"
