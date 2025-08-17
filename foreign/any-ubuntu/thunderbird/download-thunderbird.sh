0e1d5247961f21c055d3972b4857c9efd3a3d13f
Uli Heller <uli.heller@daemons-point.com>
2025-08-18 00:02:43 +0200

    thunderbird: bz2 -> bz (2)

--- foreign/any-ubuntu/thunderbird/download-thunderbird.sh
+++ foreign/any-ubuntu/thunderbird/download-thunderbird.sh
@@ -3,17 +3,17 @@
 #         https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/128.0esr/linux-x86_64/de/thunderbird-128.0esr.tar.bz2
 BASE_URL="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/"
 THUNDERBIRD_VERSION="$(curl -s "${BASE_URL}"|grep -o "/pub/thunderbird/releases/[0-9][0-9.]*[^/]*/"|grep -v "/12[579]"|grep -v "/13[0-9]"|grep -v "/14[1-9]"|grep -v "b[^/]*/$"|xargs -n1 basename|sort -V|tail -1)"
-URL="${BASE_URL}${THUNDERBIRD_VERSION}/linux-x86_64/de/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
-if [ -s "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2" ]; then
+URL="${BASE_URL}${THUNDERBIRD_VERSION}/linux-x86_64/de/thunderbird-${THUNDERBIRD_VERSION}.tar.xz"
+if [ -s "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.xz" ]; then
   echo >&2 "Bereits vorhanden: '${URL}'"
   RC=1
-#  echo "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
+#  echo "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.xz"
 #  RC=0
 else
-  echo >"${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2.urls" "${URL}"
+  echo >"${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.xz.urls" "${URL}"
   echo >&2 "Lade herunter '${URL}'"
-  curl -s "${URL}" >"${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
-  echo "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.bz2"
+  curl -s "${URL}" >"${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.xz"
+  echo "${HOME}/Downloads/thunderbird-${THUNDERBIRD_VERSION}.tar.xz"
   RC=0
 fi
 exit "${RC}"
